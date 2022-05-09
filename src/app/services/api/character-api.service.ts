4160e5caef1bb1ffe57eb0776e1e7eb1ec0080bc
Amilto Romagno <39378921+amiilto@users.noreply.github.com>
2022-05-09 10:30:10 -0300

    Cleaning up

--- src/app/services/api/character-api.service.ts
+++ src/app/services/api/character-api.service.ts
@@ -8,8 +8,8 @@ import { Md5 } from 'ts-md5/dist/md5';
 })
 export class CharacterApiService {
 
-  PUBLIC_KEY = 'd8701bd9fa7772e8315db18b9bf01621';
-  PRIVATE_KEY = 'fac133c49f4b3433477e6ae79ff252f8d2df84be';
+  PUBLIC_KEY = 'insira aqui a chave publica';
+  PRIVATE_KEY = 'insira aqui a chave privada';
 
   HASH = Md5.hashStr('1'+this.PRIVATE_KEY+this.PUBLIC_KEY);
   BASE_API_URL = `https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${this.PUBLIC_KEY}&hash=${this.HASH}`;
